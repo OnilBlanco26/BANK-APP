700037d6586343882435a62386367a942b624647
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-01 22:08:35 -0300

    We create the Server class, with its constructor method and the other methods necessary for the operation.

--- models/server.js
+++ models/server.js
@@ -1,3 +1,39 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+
+
+// 1. Creamos una clase
+
+class Server {
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT || 3000
+
+        this.paths = {
+            users: '/api/v1/users',
+            transfers: '/ap1/v1/transfers'
+        }
+    }
+
+    middlewares(){
+        
+
+        //UTILIZAMOS LAS CORS PARA PERMITIR ACCESO A LA API DESDE EL FRONT-END
+        this.app.use(cors())
+        //UTILIZAMOS EXPRESS.JSON PARA PARSEAR EL BODY DE LA REQUEST
+        this.app.use(express.json())
+    }
+
+    routes() {
+
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server running on port ', this.port)
+        })
+    }
+
+}
+
