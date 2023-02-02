f4c5d5a2490a3e999ccc17f6e5b479ae6969aaa8
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-02 17:42:45 -0300

    Creating the transfer driver and the transferAmount method

---
+++ controllers/transfers.controllers.js
@@ -0,0 +1,76 @@
+const Transfers = require("../models/transfers.model");
+const User = require("../models/users.model");
+
+const transferAmount = async (req, res) => {
+    console.log('estoy entrando en transfers')
+    const { amount, senderAccountNumber, receiverAccountNumber} = req.body
+
+    const userReceive = await User.findOne({
+        where: {
+            status: true,
+            accountNumber: receiverAccountNumber
+        }
+    })
+
+    if(!userReceive) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'This user account to which you want to send the amount does not exist.'
+        })
+    }
+
+    const userReceiveId = userReceive.id
+
+    const userTransfer = await User.findOne({
+        where: {
+            status: true,
+            accountNumber: senderAccountNumber
+        }
+    })
+
+    if(!userTransfer) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'The account of the user making the transfer does not exist.'
+        })
+    }
+
+    const userTransferId = userTransfer.id
+
+    if(userReceiveId === userTransferId) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'The user to whom you are sending is the same user who is sending'
+        })
+    }
+
+    if(amount > userTransfer.amount) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'The user cannot transfer more than the amount he has in his account.'
+        })
+    }
+
+    const newAmountUserTransfer = userTransfer.amount - amount 
+    const newAmountUserReceiver = userReceive.amount + amount 
+
+    await userTransfer.update({amount: newAmountUserTransfer})
+    await userReceive.update({amount: newAmountUserReceiver})
+
+    const transfer = await Transfers.create({
+        amount,
+        senderUserId: userTransferId,
+        receiverUserId: userReceiveId
+    })
+
+    res.status(200).json({
+        status: 'success',
+        message: 'The transaction has been successfully completed',
+        transfer
+    })
+}
+
+
+module.exports = {
+    transferAmount
+}
