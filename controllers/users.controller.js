da279b953d9c75db357d677a13e16014ae76f57e
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-01 22:48:53 -0300

    We create the file users.controllers and import the controllers and receive them in the following routes

---
+++ controllers/users.controller.js
@@ -0,0 +1,21 @@
+const User = require("../models/users.model");
+
+
+const register = async (req, res) => {
+
+}
+
+const login = async (req, res) => {
+
+}
+
+const getHistory = async (req, res) => {
+
+}
+
+
+module.exports = {
+    register, 
+    login,
+    getHistory
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
+const { getHistory, register, login } = require("../controllers/users.controller");
 
 // 1.Creamos una instancia del objeto Router
 const router = Router()
 
-router.get('/:id/history', getUserHistoryById)
+router.get('/:id/history', getHistory)
 
-router.post('/signup', createNewUserAccount)
+router.post('/signup', register)
 
-router.post('/login', userLogin)
+router.post('/login', login)
 
 
 // 2. Exportamos router
