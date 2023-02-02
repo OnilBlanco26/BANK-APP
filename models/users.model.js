f77a5e71604764182bf4dff175708a3482dc1fa7
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-01 22:34:00 -0300

    We create the model of our table for the users

---
+++ models/users.model.js
@@ -0,0 +1,36 @@
+const {DataTypes} = require('sequelize')
+const { db } = require('../database/db')
+
+// 1.definir nuestra tabla para la base de datos
+const User = db.define('user', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: DataTypes.INTEGER
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    accountNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    amount: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 1000
+    },
+    status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    }
+})
+
+module.exports = User;
