61577bbef12e1a8551849779355742c6b50b8131
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-02 17:43:48 -0300

    Creating the template for the transfer table

---
+++ models/transfers.model.js
@@ -0,0 +1,26 @@
+const {DataTypes} = require('sequelize')
+const {db} = require('../database/db')
+
+const Transfers = db.define('transfers', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: DataTypes.INTEGER
+    },
+    amount: {
+        type: DataTypes.DECIMAL(10,2),
+        allowNull: false
+    },
+    senderUserId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    receiverUserId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+    
+})
+
+module.exports = Transfers
