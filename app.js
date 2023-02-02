6d22e94f7caafbe37809d3fb5f356d84a4de9497
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-01 21:44:03 -0300

    Creating the app.js file to start our application

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+// 1. Importamos el modelo
+
+const Server = require('./models/server')
+
+// 2. Instaciamos la clase
+
+const server = new Server()
+
+// 3. Ejecutamos el metodo llisten para escuchar las peticiones
+
+server.listen()
