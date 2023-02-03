6c8f6bc22d064495b24a8cacdd3a411c37d0b519
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-03 00:17:12 -0300

    Adding the check from express-validator, importing validateFields, to validate requests

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,25 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const { getHistory, register, login } = require("../controllers/users.controller");
+
+const { validateFields } = require('../middlewares/validateField.middleware')
 
 // 1.Creamos una instancia del objeto Router
 const router = Router()
 
 router.get('/:id/history', getHistory)
 
-router.post('/signup', register)
+router.post('/signup', [
+    check('name', 'The name must be mandatory').not().isEmpty(),
+    check('password', 'The password must be mandatory').not().isEmpty(),
+    validateFields
+], register)
 
-router.post('/login', login)
+router.post('/login', [
+    check('password', 'The password must be mandatory').not().isEmpty(),
+    check('accountNumber', 'The Accont Number must be mandatory').not().isEmpty(),
+    validateFields
+] , login)
 
 
 // 2. Exportamos router
