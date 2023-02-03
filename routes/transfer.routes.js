41495336408f2b5f18a6fd0d1a5fba68de214031
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-03 00:16:46 -0300

    Adding check from express-validator, importing validateFields to validate requests

--- routes/transfer.routes.js
+++ routes/transfer.routes.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const { transferAmount } = require("../controllers/transfers.controllers");
+const { validateFields } = require("../middlewares/validateField.middleware");
 
 
 const router = Router()
 
-router.post('/', transferAmount)
+router.post('/', [
+    check('amount', 'The amount must be mandatory').not().isEmpty(),
+    check('senderAccountNumber', 'The SenderAccountNumber must be mandatory').not().isEmpty(),
+    check('receiverAccountNumber', 'The receiverAccountNumber must be mandatory').not().isEmpty(),
+    validateFields
+] , transferAmount)
 
 
 module.exports= {
